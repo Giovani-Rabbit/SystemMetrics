89cf0ec046f0eba33dd177d3759f0e68c4cee43c
Giovani Coelho <giovanicoelho2004@hotmail.com>
2026-03-12 23:37:28 -0300

    metrics thread

--- src/app.rs
+++ src/app.rs
@@ -1,29 +1,48 @@
-use std::io;
+use std::{
+    io,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, read};
 use ratatui::{DefaultTerminal, Frame};
 
 use crate::system::traits::SystemMonitor;
 
 pub struct App {
-    pub sysmon: Box<dyn SystemMonitor>,
+    pub sysmon: Arc<Mutex<Box<dyn SystemMonitor + Send>>>,
     exit: bool,
 }
 
 impl App {
-    pub fn new(sysmon: Box<dyn SystemMonitor>) -> Self {
+    pub fn new(sysmon: Box<dyn SystemMonitor + Send>) -> Self {
         Self {
-            sysmon,
+            sysmon: Arc::new(Mutex::new(sysmon)),
             exit: false,
         }
     }
 
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        self.metrics_thread();
+
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
             self.handle_events()?;
         }
+
         Ok(())
+    }
+
+    fn metrics_thread(&self) {
+        let sysmon_thread = Arc::clone(&self.sysmon);
+
+        thread::spawn(move || {
+            loop {
+                sysmon_thread.lock().unwrap().refresh();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
     }
 
     fn draw(&self, frame: &mut Frame) {
@@ -31,12 +50,14 @@ impl App {
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
+        if event::poll(Duration::from_millis(1000))? {
+            match read()? {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    self.handle_key_event(key_event)
+                }
+                _ => {}
+            };
+        }
         Ok(())
     }
 
